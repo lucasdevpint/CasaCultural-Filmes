dde8445987ada2bd7d614517252a9970516769e3
Lucas Pinto <lucasp.projects@gmail.com>
2025-05-25 21:48:43 -0300

    Commit inicial do projeto Spring Boot para Casa Cultural Av. Filmes

---
+++ casacultural-filmes/src/main/java/br/com/casacultural/casacultural_filmes/CasaculturalFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.casacultural.casacultural_filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaculturalFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaculturalFilmesApplication.class, args);
+	}
+
+}
---
+++ casacultural-filmes/src/test/java/br/com/casacultural/casacultural_filmes/CasaculturalFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casacultural.casacultural_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaculturalFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
